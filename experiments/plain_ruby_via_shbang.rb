3b279c36c56f4c0c1e1f8eaa9c4bf22055a80bf3
Michael Grosser <michael@grosser.it>
2020-02-29 16:32:38 -0800

    experiment

---
+++ experiments/plain_ruby_via_shbang.rb
@@ -0,0 +1,5 @@
+#!/Users/mgrosser/.rbenv/versions/2.6.5/bin/ruby --disable-gems
+# frozen_string_literal: true
+# can we start ruby fast with a hardcoded executable and args ?
+# ... the answer is yes :)
+puts RUBY_VERSION
